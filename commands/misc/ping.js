48e30a55ae7e774c0cae83d2d77e20cfa3a55104
UndiedHitler <hemanthchakka2007@gmail.com>
2021-06-09 14:53:39 +0530

    Description to support temporary help command

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
 	commands: ['ping', 'latency'],
+	description: 'Shows the bot\'s latency',
 	callback: (message, args, text, client) => {
 		message.reply(`🏓Latency is **${Math.round(client.ws.ping)}** ms`);
 	},
