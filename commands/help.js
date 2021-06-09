d1822837506339023c44e90038b36e5e82186b14
UndiedHitler <hemanthchakka2007@gmail.com>
2021-06-09 15:34:06 +0530

    Better help command

--- commands/help.js
+++ commands/help.js
@@ -42,7 +42,7 @@ module.exports = {
 			const args = command.expectedArgs ? ` ${command.expectedArgs}` : '';
 			const { description } = command;
 
-			reply += `**${prefix}${mainCommand}${args}** = ${description}\n`;
+			reply += `**${mainCommand}${args}** = ${description}\n`;
 		}
 
 		message.channel.send(reply);
