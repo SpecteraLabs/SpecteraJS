61b5a01cda1896506e447f1f19c4145ba0d9894c
UndiedHitler <hemanthchakka2007@gmail.com>
2021-06-10 14:54:32 +0530

    mfw back paining

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,7 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 const loadCommands = require('../load-commands');
 const { MessageEmbed } = require('discord.js');
+const commandPrefixSchema = require('../schemas/command-prefix-schema');
 
 module.exports = {
 	commands: ['help', 'h'],
@@ -17,6 +18,13 @@ module.exports = {
 				.setColor('#FFFFFF')
 				.setTimestamp();
 			message.reply({ embed : halpembed });
+		}
+		else if (args[0].toLowerCase() === 'ping' || args[0].toLowercase() === 'latency' || args[0].toLowerCase() === 'runtime') {
+			const Lembed = new MessageEmbed()
+				.setTitle('Ping | Latency | Runtime')
+				.setDescription(`\`\`\`yaml\n #Shows bot latency \n Usage: +ping\`\`\``)
+				.setTimestamp();
+			message.reply({ embed: Lembed, allowedMentions: { repliedUser: false } });
 		}
 	},
 };
