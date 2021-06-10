f4ca5fcac79c1ac7cabf0dfb2189fe6b63b557e4
Lollipop <mohammedrayyan112233@gmail.com>
2021-06-10 16:50:42 +0530

    Changed the Help command a bit

---
+++ commands/embed_color_schema/misc.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+const commandBase = require('../command-base');
+
+module.exports = {
+    commands: 'misc',
+    description: 'Embed Color Schema that should be used for Misc category',
+    callback: async (message) => {
+        // adding, wait
+    }
+};
--- commands/help.js
+++ commands/help.js
@@ -12,13 +12,25 @@ module.exports = {
 	callback: (message, args, text) => {
 		if (!args.length) {
 			const halpembed = new MessageEmbed()
-				.setTitle('Help command')
+				.setTitle('Help')
+				.addFields(
+					{ name: "Prefix", value: "`setprefix`" },
+					{ name: "Misc", value: "\`ping\` \`latency\ ` \`runtime\`" } 
+				)
+				/* I would recommend adding fields by the method above
 				.addField('Prefix', '\`setprefix\`')
 				.addField('Misc', '\`ping\` \`latency\` \`runtime\`')
+				*/
+				
 				.setColor('#FFFFFF')
 				.setTimestamp();
 			message.reply({ embed : halpembed });
 		}
+		/*
+		I wouldn't recommend adding this to every command like you did after the comment
+		See my friend's help command repository if you understand it :/
+		https://github.com/KineticTactic/KineticBot/blob/master/commands/utility/help.js
+		*/
 		else if (args[0].toLowerCase() === 'ping' || args[0].toLowerCase() === 'latency' || args[0].toLowerCase() === 'runtime') {
 			const Lembed = new MessageEmbed()
 				.setTitle('Ping | Latency | Runtime')
