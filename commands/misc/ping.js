b562b96307e284cd4ed9e913c2f96f2fa0b6840c
UndiedHitler <hemanthchakka2007@gmail.com>
2021-06-10 09:19:42 +0530

    new alias: 'runtime'

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
-	commands: ['ping', 'latency'],
+	commands: ['ping', 'latency', 'runtime'],
 	description: 'Shows the bot\'s latency',
 	callback: (message, args, text, client) => {
 		message.reply(`🏓Latency is **${Math.round(client.ws.ping)}** ms`);
