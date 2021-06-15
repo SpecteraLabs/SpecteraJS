4ec7ffc665c6bcc31956600eef574dc8a224efc4
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-15 11:26:00 +0530

    Something

--- commands/help.js
+++ commands/help.js
@@ -9,6 +9,8 @@ const commandPrefixSchema = require('../schemas/command-prefix-schema');
 module.exports = {
 	commands: ['help', 'h'],
 	description: "Describes all of this bot's commands",
+	minArgs: 0,
+	maxArgs: 2,
 	callback: (message, args, text) => {
 		if (!args.length) {
 			const halpembed = new MessageEmbed()
