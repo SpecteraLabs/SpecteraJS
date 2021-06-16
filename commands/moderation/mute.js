097499e5dba9a79c291ca59f2287b40f522339ed
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-16 16:25:38 +0530

    Better mutecommand

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -10,33 +10,32 @@ module.exports = {
 		if (!mutedPerson) {
 			message.reply('You have to tag someone to mute!');
 		}
-		const muterole = message.guild.roles.cache.find(r => r.name === 'Muted');
+		let muterole = message.guild.roles.cache.find(r => r.name === 'Muted');
 		if (!muterole) {
 			message.reply('No muterole found for this server!');
-			await message.guild.roles.create({
+			muterole = await message.guild.roles.create({
 				name: 'Muted',
 				color: '#696969',
 				reason: 'This server needed a MuteRole',
 			});
-			var array = [];
 			function getChannelIDs(fetch) {
 				const mutrole = message.guild.roles.cache.find(r => r.name === 'Muted');
 				try{
 					let channels = message.guild.channels.cache.array();
 					for (const channel of channels) {
-						array.push(channel.id);
 						channel.createOverwrite(mutrole, {
 							SEND_MESSAGES: false,
 							ADD_REACTIONS: false,
+							CONNECT: false,
 						});
 					}
+					mutedPerson.roles.add(muterole);
 				}
 				catch(err) {
 					console.log('array error');
 					message.channel.send('An error occoured while getting the channels.');
 					console.log(err);
 				}
-				return array;
 			}
 			getChannelIDs();
 			message.channel.send(`Created a muterole for this server, now you can use that role to mute people`);
