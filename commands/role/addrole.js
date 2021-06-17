5d48d7d893e7824980f687e6e56a4a40d9e30479
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-17 11:21:23 +0530

    Add role command

--- commands/role/addrole.js
+++ commands/role/addrole.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
-	commands: ['addrole'],
+	commands: ['addrole', 'role add'],
 	minArgs: 2,
 	permissions: 'MANAGE_ROLES',
 	permissionError: 'You do not have permission to run this command',
 	callback: async (message, args, text, client) => {
-		const target = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+		const target = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.displayName === args[0]) || message.guild.members.cache.find(r => r.user.tag === args[0]);
+		const trole = message.mentions.members.first() || message.guild.roles.cache.get(args[1]) || message.guild.roles.cache.find(r => r.name === args[1]) || message.guild.roles.cache.find(r => r.user.tag === args[1]);
+		if (!target && !args[0]) return message.reply('You need to either tag or specify id or specify tag of a user!');
+		if (!args[1]) return message.reply('You need to specify a role!');
+		if (!trole) return message.reply('Could not find that role!');
+		target.roles.add(trole);
+		message.reply(`Alright added role **${trole.name}** to **${target.user.tag}**`);
 	},
 };
