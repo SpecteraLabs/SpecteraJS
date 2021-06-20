3d29aeb655f6246f3bda41f59eacb24fd7624dc7
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-20 16:44:46 +0530

    Added docs/rtfm commands

---
+++ commands/misc/v13docs.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+module.exports = {
+	commands: ['v13docs', 'v13rtfm'],
+	callback: async (message, args, text, client) => {
+		if (!args) return message.reply(`Incorrect syntax, Use +docs <query>`);
+		const uri = `https://djsdocs.sorta.moe/v2/embed?src=master&q=${encodeURIComponent(args)}`;
+		const axios = require('axios');
+		axios.get(uri).then((embed) => {
+			const { data } = embed;
+			if (data && !data.error) {
+				message.reply({ embeds : [embed.data], allowedMentions: { repliedUser: false } });
+			}
+			else {
+				message.reply('No result found in documentation!');
+			}
+		});
+	},
+};
