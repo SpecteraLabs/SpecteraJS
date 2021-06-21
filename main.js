034085bf083630d42b35befde62a916545379cfb
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-21 12:05:43 +0530

    Bug Fix

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client({
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
 	intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'GUILD_MEMBERS', 'GUILD_PRESENCES', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_TYPING', 'GUILD_BANS', 'GUILD_WEBHOOKS'],
-});
+}).setMaxListeners(0);
 
 const config = require('./config.json');
 client.snipes = new Discord.Collection();
