0ee5c7f4aa7ac4ebb30c5b04d6a91899103b252d
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-22 20:41:27 +0530

    Eval command

---
+++ commands/misc/eval.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+const Discord = require('discord.js');
+module.exports = {
+	commands: ['eval'],
+	callback: async (message, args, text, client) => {
+		if (!message.author.id === "564468550727761920") return message.channel.send("cant use this");
+		const clean = taxt => {
+			if (typeof (taxt) === "string") return text.replaceAll('`', "`" + String.fromCharCode(8203)).replaceAll('@', "@" + String.fromCharCode(8203));
+			else return taxt;
+		};
+		try {
+			var result = args.join(" ");
+			let noResultArg = new Discord.MessageEmbed()
+				.setColor("#e31212")
+				.setDescription("ERROR: No valid eval args were provided");
+			if (!result) return message.channel.send({ embeds : [noResultArg] });
+			let evaled = eval(result);
+
+
+			let resultSuccess = new Discord.MessageEmbed()
+				.setColor("#8f82ff")
+				.setTitle("Success")
+				.addField(`Input:\n`, '```js\n' + `${args.join(' ')}` + '```', false)
+				.addField(`Output:\n`, '```js\n' + clean(evaled) + '```', true);
+
+			message.channel.send({ embeds: [resultSuccess] });
+		}
+		catch (err) {
+			let resultError = new Discord.MessageEmbed()
+				.setColor("#e31212")
+				.setTitle("An error has occured")
+				.addField(`Input:\n`, '```js\n' + `${result}` + '```', false)
+				.addField(`Output:\n`, '```js\n' + `${clean(err)}` + '```', true);
+			return message.channel.send({ embeds : [resultError] });
+		}
+	},
+};
