661bd478c00c676cde735ffa6269ce4f7effbc6f
UndiedGamer <hemanthchakka2007@gmail.com>
2021-06-22 20:47:40 +0530

    Surprise

--- events/ready.js
+++ events/ready.js
@@ -7,10 +7,12 @@ module.exports = {
 
 		loadCommands(client);
 		commandBase.loadPrefixes(client);
+		const types = ['WATCHING', 'LISTENING', 'COMPETING', 'STREAMING', 'PLAYING'];
 		const activities = [`${client.guilds.cache.size} servers`, '+help for list of commands', 'Join https://discord.gg/WaqYbeFQUb for support'];
 		setInterval(function() {
+			const typo = types[Math.floor(Math.random() * types.length)];
 			const randAact = activities[Math.floor(Math.random() * activities.length)];
-			client.user.setActivity(randAact, { type: "WATCHING" });
+			client.user.setActivity(randAact, { type: typo });
 		}, 2800);
 		console.log(`${client.user.tag} is ready on ${client.guilds.cache.size} servers and ${client.users.cache.size} members using it`);
 	},
