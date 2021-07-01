c4e3b203791a0bbb8f8365c53721b4d47f6626d0
UndiedGamer <hemanthchakka2007@gmail.com>
2021-07-01 13:08:59 +0530

    Message Logs v0.5.0

---
+++ commands/logs/message-logger.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+const mongo = require("../../mongo");
+const messageLogSchema = require("../../schemas/message-log-schema");
+const Discord = require('discord.js');
+
+module.exports = {
+	commands: ['messagelogs', 'mlogs'],
+	permission: 'ADMINISTRATOR',
+	permissionError: 'You cannot set message log channel!',
+	callback: async (message, args, text, client) => {
+		if (!args.length) return;
+		const target = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]);
+		if (!target) return message.reply('Couldn\'t find that channel');
+		const guildId = message.guild.id;
+		await mongo().then(async (mongoose) => {
+			await messageLogSchema.findOneAndUpdate({
+				_id: guildId,
+			},
+			{
+				_id: guildId,
+				channelId: target.id,
+			},
+			{
+				upsert: true,
+			});
+			const succEmbed = new Discord.MessageEmbed()
+				.setDescription(`${client.emotes.success} Message logging channel for this server now is ${target}`)
+				.setColor(client.colors.success)
+				.setTimestamp();
+			message.reply({ embeds: [succEmbed], allowedMentions: { repliedUser: false } });
+		});
+	},
+};
--- commands/role/createrole.js
+++ commands/role/createrole.js
@@ -1,51 +1,54 @@
 /* eslint-disable no-unused-vars */
 module.exports = {
-  commands: ["createrole", "crole"],
-  minArgs: 1,
-  permissions: "MANAGE_ROLES",
-  permissionError: "You donot have permissions to create a role!",
-  callback: async (message, args, text, client) => {
-    const reason = args.join(" ").slice(22);
-    if (!args[0]) {
-      message.reply(`+crole RoleName RoleColor Mentionable(true/false)
+	commands: ["createrole", "crole"],
+	minArgs: 1,
+	permissions: "MANAGE_ROLES",
+	permissionError: "You donot have permissions to create a role!",
+	callback: async (message, args, text, client) => {
+		const reason = args.join(" ").slice(22);
+		if (!args[0]) {
+			message.reply(`+crole RoleName RoleColor Mentionable(true/false)
 								  ^^^^^^^^
 								  RoleName is a required Argument
 			`);
-    }
-    if (!args[1]) {
-      await message.guild.roles.create({
-        commands: `${args[0]}`,
-      });
-      const rolie = message.guild.roles.cache.find((r) => r.name === args[0]);
-      message.reply(`Succesuflly created role **${rolie.name}**`);
-    } else if (!args[1].startsWith("#")) {
-      message.reply(`You need to specify a hex! or dont include a color`);
-    }
-    if (!args[2]) {
-      await message.guild.roles.create({
-        commands: args[0],
-        color: args[1],
-      });
-      const rolie = message.guild.roles.cache.find((r) => r.name === args[0]);
-      message.reply(`Succesuflly created role **${rolie.name}**`);
-    } else if (
-      args[2].toLowerCase() === "true" ||
+		}
+		if (!args[1]) {
+			await message.guild.roles.create({
+				commands: `${args[0]}`,
+			});
+			const rolie = message.guild.roles.cache.find((r) => r.name === args[0]);
+			message.reply(`Succesuflly created role **${rolie.name}**`);
+		}
+		else if (!args[1].startsWith("#")) {
+			message.reply(`You need to specify a hex! or dont include a color`);
+		}
+		if (!args[2]) {
+			await message.guild.roles.create({
+				commands: args[0],
+				color: args[1],
+			});
+			const rolie = message.guild.roles.cache.find((r) => r.name === args[0]);
+			message.reply(`Succesuflly created role **${rolie.name}**`);
+		}
+		else if (
+			args[2].toLowerCase() === "true" ||
       args[2].toLowerCase() === "false"
-    ) {
-      await message.guild.roles.create({
-        commands: args[0],
-        color: args[1],
-        mentionable: args[2].toLowerCase(),
-      });
-      const rolie = message.guild.roles.cache.find((r) => r.name === args[0]);
-      message.reply(`Succesuflly created role **${rolie.name}**`);
-    } else if (
-      !args[2].toLowerCase() === "true" ||
+		) {
+			await message.guild.roles.create({
+				commands: args[0],
+				color: args[1],
+				mentionable: args[2].toLowerCase(),
+			});
+			const rolie = message.guild.roles.cache.find((r) => r.name === args[0]);
+			message.reply(`Succesuflly created role **${rolie.name}**`);
+		}
+		else if (
+			!args[2].toLowerCase() === "true" ||
       !args[2].toLowerCase() === "false"
-    ) {
-      message.reply(
-        "You need to choose either true or false as third argument"
-      );
-    }
-  },
+		) {
+			message.reply(
+				"You need to choose either true or false as third argument",
+			);
+		}
+	},
 };
